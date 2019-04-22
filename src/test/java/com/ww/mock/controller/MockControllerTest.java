ae11f2b66509a69d3f3e0fbe0e3caca08b529114
SunZhongWei <littlesheepw@gmail.com>
2019-04-22 14:17:31 +0800

    更新注释内容

--- src/test/java/com/ww/mock/controller/MockControllerTest.java
+++ src/test/java/com/ww/mock/controller/MockControllerTest.java
@@ -16,8 +16,9 @@ import org.springframework.test.web.servlet.MvcResult;
 
 import static org.mockito.BDDMockito.any;
 import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 /**
  * @Program: study-mockmvc
@@ -33,6 +34,10 @@ public class MockControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    /**
+     * 不加此bean对象启动即会报错，因为controller层中依赖此bean对象
+     * 这里的主要作用是：使用mock对象代替原来spring的bean，然后模拟底层数据的返回，而不是调用原本真正的实现。
+     */
     @MockBean
     private MockService mockService;
 
@@ -54,12 +59,21 @@ public class MockControllerTest {
     /**
      * 测试MockMVC是否只走controller层，不走下层。
      * 答案是对的，我在controller层中调用了service层中一个sout方法进行打印，但是控制台中并没有输出那句话。
+     *
+     * MockMvcRequestBuilders.get()返回一个ResultActions对象。
+     * ResultActions.param添加请求传值，可以带多个参数。
+     * ResultActions.accept(MediaType.TEXT_HTML_VALUE))设置返回类型。
+     * ResultActions.andExpect添加执行完成后的断言，验证执行结果是否正确。
+     * ResultActions.andDo添加一个MockMvcResultHandlers结果处理器，表示要对结果做点什么事情，如此处使用MockMvcResultHandlers.print()输出整个响应结果信息。
+     * ResultActions.andReturn表示执行完成后返回相应的结果。
+     *
      * @throws Exception
      */
     @Test
     public void getAuthUser() throws Exception{
         MvcResult result = mockMvc.perform(get("/mock/authUser/16"))
                 .andExpect(status().isOk())
+                .andDo(print())
                 .andReturn();
         log.info("[getAuthUser] [result:{}]", result.getResponse().getContentAsString());
     }
