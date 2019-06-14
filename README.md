c5ca3c6cd302d061620690ede4cef5e1f34cc2e5
SunZhongWei <littlesheepw@gmail.com>
2019-06-14 18:58:40 +0800

    Update class comment adn README.md

--- README.md
+++ README.md
@@ -3,7 +3,10 @@
 本项目使用使用SpringBoot2.13+JPA，主要目的是学习MockMVC。  
 1、首先在Mysql中创建名称为sunshine数据库，当然也可以修改application.properties中spring.datasource.url连接地址为你自己的。  
 2、点击MockApplication启动类启动项目，数据表会自动进行创建，不过数据需要自行添加。(本项目要不要数据表无所谓，因为根本不会跟数据持久层交互，也就是说不会从数据库中取出真实数据进行返回，返回的都是mock的数据)。  
-3、使用postman进行测试，restful风格url。
+3、使用postman进行测试，restful风格url。  
+
+本项目的重点在于MockControllerTest类中的方法以及注释。  
+`推荐文章:` https://www.baeldung.com/integration-testing-in-spring  
 
 如果你有更好的建议请联系我。不管你star不star，我先点为敬咯。
 
--- src/test/java/com/ww/mock/controller/MockControllerTest.java
+++ src/test/java/com/ww/mock/controller/MockControllerTest.java
@@ -13,26 +13,31 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import static org.mockito.BDDMockito.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.mockito.BDDMockito.any;
+import static org.mockito.BDDMockito.given;
 
 /**
- * @Program: study-mockmvc
- * @Description: 测试MockController，学习使用MockMVC模拟测试
+ * 测试MockController，学习使用MockMVC模拟测试
  *
- * 在此之前思考一个问题:@SpringBootTest 和 @WebMvcTest注解的区别？
- *
- * WebMvcTest：用于单元测试场景，需要指定待测试的Controller；他也会启动一个Spring容器，但只会加载其中部分MVC相关的内容，
+ * <b>problem<b/>: @SpringBootTest 和 @WebMvcTest注解的区别?
+ * `@WebMvcTest`：
+ * 主要用于controller层测试，只覆盖应用程序的controller层，HTTP请求和响应是Mock出来的，因此不会创建真正的连接。
+ * 需要创建 MockMvc bean进行模拟接口调用，如果Controller层对Service层中的其他bean有依赖关系，那么需要使用Mock提供所需的依赖项。
+ * 需要指定待测试的Controller；他也会启动一个Spring容器，但只会加载其中部分MVC相关的内容，
  * 不会加载的bean：Component、Service或者Repository等注解的Bean。
  * 会加载的bean：Controller/ControllerAdvice/JsonComponent/Converter/Filter/WebMvcConfigurer/HandlerMethodArgumentResolver等。
  *
- * SpringBootTest：用于集成测试场景，不需要指定待测试的Controller；它会启动一个完整的Spring容器，所有的Bean都会进行加载与初始化。
- * @Author: Sun
- * @Create: 2019-04-22 11:35
- * @Version: 1.0
+ * `@SpringBootTest`：用于集成测试场景，不需要指定待测试的Controller，他会告诉Spring Boot去寻找一个
+ * 主配置类（例如一个带@SpringBootApplication的类），并使用它来启动Spring应用程序上下文。SpringBootTest
+ * 加载完整的应用程序并注入所有可能很慢的bean。
+ *
+ * @author: Sun
+ * @create: 2019-04-22 11:35
+ * @version: 1.0
  **/
 @RunWith(SpringRunner.class)
 @WebMvcTest(MockController.class)
@@ -64,23 +69,24 @@ public class MockControllerTest {
     }
 
     /**
-     * 测试MockMVC是否只走controller层，不走下层。
-     * 答案是对的，我在controller层中调用了service层中一个sout方法进行打印，但是控制台中并没有输出那句话。
+     * 执行流程:
+     * MockMvcRequestBuilders.get() 返回一个MockHttpServletRequestBuilder对象;
+     * MockHttpServletRequestBuilder.param() 添加请求传值，可以带多个参数; (这里并没有使用到)
+     * MockHttpServletRequestBuilder.accept(MediaType.TEXT_HTML_VALUE)) 设置返回类型; (这里并没有使用到)
      *
-     * MockMvcRequestBuilders.get()返回一个ResultActions对象。
-     * ResultActions.param添加请求传值，可以带多个参数。
-     * ResultActions.accept(MediaType.TEXT_HTML_VALUE))设置返回类型。
-     * ResultActions.andExpect添加执行完成后的断言，验证执行结果是否正确。
-     * ResultActions.andDo添加一个MockMvcResultHandlers结果处理器，表示要对结果做点什么事情，如此处使用MockMvcResultHandlers.print()输出整个响应结果信息。
+     * mockMvc.perform()方法，执行一个RequestBuilder请求，调用controller的业务处理逻辑，perform()方法返回ResultActions，返回操作结果，通过ResultActions，提供了统一的验证方式;
+     *
+     * ResultActions.andExpect添加执行完成后的断言，验证执行结果是否正确;
+     * ResultActions.andDo添加一个MockMvcResultHandlers结果处理器，表示要对结果做点什么事情，如此处使用MockMvcResultHandlers.print()输出整个响应结果信息;
      * ResultActions.andReturn表示执行完成后返回相应的结果。
      *
      * @throws Exception
      */
     @Test
     public void getAuthUser() throws Exception{
-        MvcResult result = mockMvc.perform(get("/mock/authUser/16"))
-                .andExpect(status().isOk())
-                .andDo(print())
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/mock/authUser/16"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andDo(MockMvcResultHandlers.print())
                 .andReturn();
         log.info("[getAuthUser] [result:{}]", result.getResponse().getContentAsString());
     }
